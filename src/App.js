1a33973bdf593d66eb235cd9854e57d64b057e79
Tausif Sadid <tausif@openhaus.in>
2024-02-16 20:55:33 +0600

    feat: Login page UI

--- src/App.js
+++ src/App.js
@@ -1,65 +1,39 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Cash from './Cash.svg';
 
 function App() {
   return (
     <div className='Home'>
       <div className='sticky-header'>
-        <h2>Open Split</h2>
+        <span className='fw-bold'>Open Split</span>
       </div>
-      <div className='due-amount'>
-        <span className='text-green'>You are owed BDT15, Check activity</span>
-      </div>
-      <div className='transaction-history'>
-        <div className='transaction-by-day'>
-          <h3 className='transaction-date'>January 2024</h3>
-          <div className='transaction-details'>
-            <div className='transaction-day'>Jan 30</div>
-            <div className='transaction-type'>
-              <img src={Cash} alt='Cash SVG' height={50} width={50} />
-            </div>
-            <div className='transaction-info'>
-              <div className='transaction-info-header'>Lunch</div>
-              <div className='transaction-info-footer'>
-                Tausif Sadid paid BDT500
-              </div>
-            </div>
-          </div>
-          <div className='transaction-details'>
-            <div className='transaction-day'>Jan 30</div>
-            <div className='transaction-type'>
-              <img
-                className='px-5'
-                src={Cash}
-                alt='Cash SVG'
-                height={50}
-                width={50}
-              />
-            </div>
-            <div className='transaction-info'>
-              <div className='transaction-info-header'>Dinner</div>
-              <div className='transaction-info-footer'>Mahir paid BDT1500</div>
-            </div>
-          </div>
-          <div className='transaction-details'>
-            <div className='transaction-day'>Jan 26</div>
-            <div className='transaction-type'>
-              <img
-                className='px-5'
-                src={Cash}
-                alt='Cash SVG'
-                height={50}
-                width={50}
-              />
-            </div>
-            <div className='transaction-info'>
-              <div className='transaction-info-header'>Netflix</div>
-              <div className='transaction-info-footer'>
-                2 people paid BDT2500
-              </div>
-            </div>
+      <div className='sign-in-page w-90 '>
+        <div className='center-div mt-4 font-large fw-bold'>Welcome Back</div>
+        <input className='input-div mt-3' placeholder='username'></input>
+        <input
+          className='input-div mt-3'
+          placeholder='password'
+          type='password'
+        ></input>
+        <div className='text-left'>
+          <div className='text-dimmed text-decoration-underline hyperlink mt-3'>
+            Forgot password?
           </div>
         </div>
+        <div className='btn-primary mt-3 fw-bold'>Log in</div>
+        <div className='center-div fw-bold hyperlink mt-3 fw-bold'>
+          New user? Sign Up
+        </div>
+      </div>
+      <div className='sign-up-page w-90 d-none'>
+        <div className='center-div mt-4 font-large fw-bold'>Welcome</div>
+        <input className='input-div mt-3' placeholder='name'></input>
+        <input className='input-div mt-3' placeholder='email'></input>
+        <input className='input-div mt-3' placeholder='username'></input>
+        <input className='input-div mt-3' placeholder='password'></input>
+
+        <div className='btn-primary mt-3 fw-bold mt-4'>Sign up</div>
       </div>
     </div>
   );
